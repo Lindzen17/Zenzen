d1e584db2a1bddb0773197cf9edbb60106d7150f
Zenzen <cabuyadaoreizen@gmail.com>
2025-04-21 02:14:53 -0400

    Add files via upload

---
+++ build/Basic_HTML_and_Javascripts_1/Basic_Javascript_1.js
@@ -0,0 +1,6 @@
+alert("Hello, World! I am an alert box!");
+function Zen() {
+    var String = "Hello Zenzen";
+    var result = String.fontcolor("red");
+    document.getElementById("Zenzen").innerHTML=result;
+}
